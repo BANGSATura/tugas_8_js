461d394e7a75076d7d1b9a64b9560498dd3a3270
STR <saturasstotenkopf@gmail.com>
2019-09-12 09:23:55 +0700

    Tugas 8 Javascript
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas8.js
@@ -0,0 +1,12 @@
+function fungsi() {
+  var Data = {
+    Nama : "Satura",
+    Umur : 20
+  }
+  console.log(Data);
+  Data.Alamat = "Jl Raya Kresek"
+  Data.Nama = "Sai";
+  console.log("Setelah dirubah");
+  console.log(Data);
+}
+fungsi()
